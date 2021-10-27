83b0149fcf33579197a77fbacfd220be0ff7cf27
Ygor Sad <ysads2010@gmail.com>
2021-10-27 18:23:52 -0300

    ep7: implement print, min, search and insert for treap

---
+++ ep7-splay-treap/treap/test_treap.py
@@ -0,0 +1,25 @@
+from treap import *
+
+t = treap()
+treap_insert(t, 11)
+treap_insert(t, 10)
+treap_insert(t, 9)
+print(treap_min(t))
+
+treap_insert(t, 7)
+treap_insert(t, 5)
+print(treap_min(t))
+
+treap_insert(t, 4)
+treap_insert(t, 3)
+print(treap_min(t))
+
+treap_insert(t, 1)
+treap_print(t)
+
+print(treap_min(t))
+print(treap_search(t, 3))   # True
+print(treap_search(t, 1))   # True
+print(treap_search(t, 11))  # True
+print(treap_search(t, 0))   # False
+print(treap_search(t, 13))  # False
---
+++ ep7-splay-treap/treap/treap.py
@@ -0,0 +1,186 @@
+"""
+Nome: Ygor Sad Machado
+NUSP: 8910368
+
+WARNING: This program requires python 3.x!
+"""
+import pdb
+from sys import maxsize
+from random import randint, seed
+
+seed(5000)
+
+class Node:
+  def __init__(self, left=None, right=None, value=None, parent=None, priority=0):
+    self.left = left
+    self.right = right
+    self.value = value
+    self.parent = parent
+    self.priority = priority
+
+  def __str__(self):
+    parent_value = self.parent.value if self.parent else "-"
+    return f"{self.value} [{self.priority}]"
+
+
+class Treap:
+  def __init__(self):
+    self.root = None
+
+# =========================================
+# Utilitary functions
+# =========================================
+
+def print_in_levels(r, level):
+  """
+  Recursively print the BST in a tree-like structure but rotated
+  90º counterclockwise – ie, given a node, its left children are
+  printed to the bottom, while its right children are on top of it.
+  """
+  if r != None:
+    print_in_levels(r.right, level + 1)
+    print(' ' * 8 * level + '->', r)
+    print_in_levels(r.left, level + 1)
+
+
+def set_parent(u, parent):
+  """
+  Safe way to update a node's parent regardless of null pointers.
+  """
+  if u is None:
+    return
+
+  u.parent = parent
+
+
+def update_child_on_parent(old, new):
+  """
+  Updates a ref to child node, taking into account whether that was a left
+  or a right child, when that child changes.
+  """
+  if old.parent:
+    if old.parent.left == old:
+      old.parent.left = new
+    else:
+      old.parent.right = new
+
+
+def rotate_right(u):
+  v = u.left
+  u.left = v.right
+  v.right = u
+
+  update_child_on_parent(u, v)
+  set_parent(v, u.parent)
+  set_parent(u, v)
+  set_parent(u.left, u)
+
+  return v
+
+
+def rotate_left(u):
+  v = u.right
+  u.right = v.left
+  v.left = u
+
+  update_child_on_parent(u, v)
+  set_parent(v, u.parent)
+  set_parent(u, v)
+  set_parent(u.right, u)
+
+  return v
+
+
+def heapify(u):
+  """
+  Uses max-heap property to rearrange the treap elements. It always returns the heap's
+  updated root.
+  """
+  while u.parent != None:
+    if u.priority > u.parent.priority:
+      if u == u.parent.left:
+        new_u = rotate_right(u.parent)
+      else:
+        new_u = rotate_left(u.parent)
+
+      u = new_u
+    else:
+      u = u.parent
+
+  return u
+
+
+def rand_priority():
+  """
+  Generates an integer between 0 and 100, assumed to be the max capacity of the treap
+  for the sake of testing.
+  """
+  return randint(0, 100)
+
+
+
+def insert_node(u, new_node, parent):
+  if u is None:
+    new_node.parent = parent
+    return new_node
+
+  if new_node.value <= u.value:
+    u.left = insert_node(u.left, new_node, u)
+  else:
+    u.right = insert_node(u.right, new_node, u)
+
+  return u
+
+
+# =========================================
+# Interface functions
+# =========================================
+
+def treap():
+  return Treap()
+
+
+def treap_insert(t, x):
+  new_node = Node(priority=rand_priority(), value=x)
+  insert_node(t.root, new_node, None)
+  t.root = heapify(new_node)
+
+
+def treap_delete(t, x):
+  return t
+
+
+def treap_search(t, x):
+  u = t.root
+
+  while u != None:
+    if x == u.value:
+      return True
+    elif x <= u.value:
+      u = u.left
+    else:
+      u = u.right
+
+  return False
+
+
+def treap_min(t):
+  u = t.root
+  if u is None:
+    return u
+
+  while u.left is not None:
+    u = u.left
+
+  return u.value
+
+
+def treap_print(t):
+  print('\n')
+
+  if t.root is None:
+    print('-> ')
+  else:
+    print_in_levels(t.root, level=0)
+
+  print('\n')
