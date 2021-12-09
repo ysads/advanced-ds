9bce000e26152da2b5eb80dbfabe30d3fbfaed95
Ygor <ysads2010@gmail.com>
2021-12-09 20:24:57 +0100

    ep9: refactor tests and calculate the extended llcp and rlcp vectors

--- ep9-pattern-search/search.py
+++ ep9-pattern-search/search.py
@@ -35,10 +35,13 @@ def suffixes_vector(T):
 
 def lcp_vector(T, sv):
   """
-  Naïvely builds the LCP vector by comparing preffixes between two consecutive
-  elements in the suffix vector.
+  Naïvely builds the LCP vector by comparing prefixes between two consecutive
+  elements in the suffix vector. For the sake of simplicity, we assume:
+    - lcp length should be the same as sv's.
+    - lcp[0] is not defined because there is no element before 0.
+    - lcp[1] is 0 since `$` is always the first suffix and it has no prefix.
   """
-  lcp = [0]
+  lcp = [0, 0]
 
   for i in range(1, len(sv)-1):
     length = 0
@@ -52,3 +55,33 @@ def lcp_vector(T, sv):
     lcp.append(length)
 
   return lcp
+
+
+def lrlcp(lcp, llcp, rlcp, i, j, side=None):
+  """
+  Recursively calculates the extended LCP vectors by simulating the binary
+  searches we may do later. Note that each m maybe only be the center of a
+  single interval delimited by a pair (i,j). Also it's important to know in
+  which direction is the search going to (left/right), hence the need for side.
+  """
+  if i == j - 1:
+    value = lcp[j]
+  else:
+    m = (i + j) // 2
+    lrlcp(lcp, llcp, rlcp, i=i, j=m, side='L')
+    lrlcp(lcp, llcp, rlcp, i=m, j=j, side='R')
+    value = min(llcp[m], rlcp[m])
+
+  if side == 'L':
+    llcp[j] = value
+  if side == 'R':
+    rlcp[i] = value
+
+
+def extended_lcp_vectors(lcp):
+  llcp = [None for _ in range(len(lcp))]
+  rlcp = [None for _ in range(len(lcp))]
+
+  lrlcp(lcp, llcp, rlcp, i=0, j=len(lcp)-1)
+
+  return llcp, rlcp
--- ep9-pattern-search/test.py
+++ ep9-pattern-search/test.py
@@ -1,24 +1,47 @@
 from search import *
 from pprint import pprint
 
+
+def dump(label, vector):
+  print("\n*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* ")
+  print(f"{label} •• {len(vector)}")
+  pprint(vector)
+
+
 def test_0():
   T = open("test_0.txt", "r").read()
   
   print(f"{T} ••• [{len(T)}]")
-  print("\nßßß *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* ßßß")
+  print("\n*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* ")
 
   sv = suffixes_vector(T)
+  dump("sv", sv)
   
-  print("\nßßß *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* ßßß")
-  print(pprint(sv))
-
   lcp = lcp_vector(T, sv)
-  print("\nßßß *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* ßßß")
-  print(pprint(lcp))
+  dump("lcp", lcp)
+
+  llcp, rlcp = extended_lcp_vectors(lcp)
+  dump("llcp", llcp)
+  dump("rlcp", rlcp)
+
 
 
 def test_1():
   T = open("test_1.txt", "r").read()
 
+  print(f"{T} ••• [{len(T)}]")
+  print("\n*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* ")
+
+  sv = suffixes_vector(T)
+  dump("sv", sv)
+
+  lcp = lcp_vector(T, sv)
+  dump("lcp", lcp)
+
+  llcp, rlcp = extended_lcp_vectors(lcp)
+  dump("llcp", llcp)
+  dump("rlcp", rlcp)
+
 
 test_0()
+# test_1()
